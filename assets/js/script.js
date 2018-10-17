776234cdb0fc99e693c3b1a74001920651d8f55b
ERaivis <38808365+ERaivis@users.noreply.github.com>
2018-10-17 22:21:25 +0300

    Todo app

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+$("ul").on("click","li",function(){
+    $(this).toggleClass("completed");
+});
+
+
+//click on x to delete
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+event.stopPropagation();
+
+});
+
+
+$("input[type='text']").keypress(function(event){
+
+    if(event.which === 13){
+        //grab text
+     var todoText =  $(this).val();
+     $(this).val("");
+        //create new li add to ul
+        $("ul").append("<li> <span><i class='fa fa-trash'></i></span> " +todoText+"</li>");
+    }
+
+
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+})
